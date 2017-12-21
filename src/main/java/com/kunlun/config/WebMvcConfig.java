3fea73464c8cad15814b4fec1d1c348294b5e9b3
Jarven <769558579@qq.com>
2017-12-21 13:39:56 +0800

    创建订单日志 创建积分日志 订单日志列表 积分日志列表

--- src/main/java/com/kunlun/config/WebMvcConfig.java
+++ src/main/java/com/kunlun/config/WebMvcConfig.java
@@ -3,9 +3,12 @@ package com.kunlun.config;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -21,6 +24,11 @@ import java.util.List;
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
     /**
      * 消息转换
