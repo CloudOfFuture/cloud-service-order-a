4b7cf9f38fe0b0c4dd3e01d6230eea6170081941
Jarven <769558579@qq.com>
2017-12-21 14:02:06 +0800

    测试服务之间互相调用

--- src/main/java/com/kunlun/api/controller/IndexController.java
+++ src/main/java/com/kunlun/api/controller/IndexController.java
@@ -1,8 +1,11 @@
 package com.kunlun.api.controller;
 
+import com.kunlun.api.service.IndexService;
 import com.kunlun.result.DataRet;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -15,10 +18,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("index")
 public class IndexController {
 
+    @Autowired
+    private IndexService indexService;
 
     @GetMapping("/test")
-    public DataRet<String> test(){
+    public DataRet<String> test() {
         return new DataRet<>("这是服务A");
+    }
+
+    /**
+     * 测试服务之间调用
+     *
+     * @param orderId
+     * @return
+     */
+    @GetMapping("/log")
+    public DataRet<String> logTest(@RequestParam(value = "order_id") Long orderId) {
+        return indexService.logTest(orderId);
     }
 
 }
